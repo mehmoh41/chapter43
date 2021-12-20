cc1890074aab7b36b7d873f09756769cf58a5c7c
Yahya Makarim <ymakarim@gmail.com>
2021-12-20 23:47:25 +0500

    menu done

--- components/menu.js
+++ components/menu.js
@@ -7,7 +7,7 @@ export default function Menu() {
       className='flex flex-col gap-8 items-center text-center h-screen w-screen justify-center bg-cover bg-opacity-25 bg-no-repeat bg-center'
       style={{ backgroundImage: `url(./assets/5.gif` }}
     >
-      <Link href=''>
+      <Link href='/home'>
         <a className='opacity-80 hover:opacity-100'>
           <h3 className='text-8xl font-bold'>WORK</h3>
           <p className='italic font-secondary opacity-80 text-xl'>
@@ -15,13 +15,13 @@ export default function Menu() {
           </p>
         </a>
       </Link>
-      <Link href=''>
+      <Link href='/about'>
         <a className='opacity-80 hover:opacity-100'>
           <h3 className='text-8xl font-bold'>ABOUT</h3>
           <p className='italic font-secondary opacity-80 text-xl'>Get to know our culture</p>
         </a>
       </Link>
-      <Link href=''>
+      <Link href='/contact'>
         <a className='opacity-80 hover:opacity-100'>
           <h3 className='text-8xl font-bold'>CONTACT</h3>
           <p className='italic font-secondary opacity-80 text-xl'>Feel free to reach out</p>
