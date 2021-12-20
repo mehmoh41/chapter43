4a3d0d679099d12b7db3c8ff21d84d6fee748ca9
mehdi mohammadi <moh.meh41@gmail.com>
2021-12-20 18:21:03 +0500

    few new changes added

--- components/Overlay.jsx
+++ components/Overlay.jsx
@@ -46,9 +46,9 @@ export default function Overlay({className}) {
             viewBox="0 0 48 48"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M41,14H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,14Z" fill="#000"/>
-            <path d="M41,26H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,26Z" fill="#000" />
-            <path d="M41,38H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,38Z" fill="#000" />
+            <path d="M41,14H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,14Z" fill="#fff"/>
+            <path d="M41,26H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,26Z" fill="#fff" />
+            <path d="M41,38H7a2,2,0,0,1,0-4H41A2,2,0,0,1,41,38Z" fill="#fff" />
           </svg>
       </>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-import '../styles/slider.scss'
+// import '../styles/slider.css'
 import '../styles/app.scss'
+import '../styles/music-wave.scss'
+// import '../components/slider'
+// At the top of _app.tsx or your individual page:
 
 
 function MyApp({ Component, pageProps }) {
