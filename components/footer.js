77743a35bd643ced4bab7667d6c218f0f4184291
Yahya Makarim <ymakarim@gmail.com>
2021-12-20 23:09:55 +0500

    xp done

---
+++ components/footer.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+export default function Footer() {
+  return (
+    <div
+      id='footer'
+      className='absolute bottom-12 left-12 right-12 flex items-center justify-between'
+    >
+      <Link href='/xp'>
+        <a>
+          <svg className='w-5 h-5 text-white' fill='currentColor' viewBox='0 0 20 20'>
+            <path d='M6.803 18.998c-.194-.127 3.153-7.16 3.038-7.469-.116-.309-3.665-1.436-3.838-1.979-.174-.543 7.007-8.707 7.196-8.549.188.158-3.129 7.238-3.039 7.469.091.23 3.728 1.404 3.838 1.979.111.575-7.002 8.676-7.195 8.549z' />
+          </svg>
+        </a>
+      </Link>
+      <span className='italic text-lg font-secondary'>Click to transform</span>
+
+      <svg viewBox='0 0 24 24' className='text-white h-5 w-5' fill='currentColor'>
+        <path d='M11 6H13V18H11V6Z' fill='currentColor' />
+        <path d='M7 13H9V18H7V13Z' fill='currentColor' />
+        <path d='M15 9H17V18H15V9Z' fill='currentColor' />
+      </svg>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import Head from "next/head";
+import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Home () {
+export default function Home() {
   return (
     <>
       <Head>
         <title>Welcome to chapter 43</title>
       </Head>
-      <h2 className="text-4xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-          WELCOME TO CHAPTER 43
+      <h2 className='text-4xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 '>
+        WELCOME TO CHAPTER 43
       </h2>
     </>
   )
