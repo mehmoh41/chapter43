eb05ac6b931ad19eb3d1dee8258817524ca2701b
mehdi mohammadi <moh.meh41@gmail.com>
2021-12-20 19:17:53 +0500

    ready for the fight

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,13 @@
-import Layout from '../components/Layout'
+
 import '../styles/globals.css'
-// import '../styles/slider.css'
-import '../styles/app.scss'
-import '../styles/music-wave.scss'
-// import '../components/slider'
-// At the top of _app.tsx or your individual page:
+
 
 
 function MyApp({ Component, pageProps }) {
   return(
-    <Layout>
+
       <Component {...pageProps} />
-    </Layout>
+
   )
 }
 
