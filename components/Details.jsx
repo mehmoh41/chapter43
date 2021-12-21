25afee8b5c1b93cb6b1c088b336514a8ab3ccb73
mehdi mohammadi <moh.meh41@gmail.com>
2021-12-21 10:59:11 +0500

    few changes in menu

--- components/Details.jsx
+++ components/Details.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 export default function Details() {
-    return (
-        <div>
-            working
-        </div>
-    )
+  const el = useRef();
+
+  useEffect(() => {
+    gsap.to(el.current, { x: "+=36" });
+  });
+  return (
+    <div>
+      <svg
+        ref={el}
+        width="32px"
+        height="32px"
+        viewBox="0 0 16 16"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path fill="#fff" d="M0 7h16v1h-16v-1z"></path>
+      </svg>
+    </div>
+  );
 }
